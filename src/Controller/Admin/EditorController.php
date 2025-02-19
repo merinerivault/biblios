ac3dbe8dbfc7d820f44fd5df4926d5a22faaf874
merine <ambre92000@hotmail.com>
2025-02-19 10:59:02 +0100

    Ajout EditorController

---
+++ src/Controller/Admin/EditorController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Editor;
+use App\Form\EditorType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/editor')]
+class EditorController extends AbstractController{
+    #[Route('', name: 'app_admin_editor')]
+    public function index(): Response
+    {
+        return $this->render('admin/editor/index.html.twig', [
+            'controller_name' => 'Admin/EditorController',
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_editor_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $editor = new Editor();
+        $form = $this->createForm(EditorType::class, $editor);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //faire quelque chose
+        }
+
+        return $this->render('admin/editor/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
